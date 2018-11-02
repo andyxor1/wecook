735b9ba7bf5b0d07f42279bf10e78e51c7bf6cea
Kaung Yang <kky006@ucsd.edu>
2018-11-01 17:15:19 -0700

    share recipe page modified with andy's wireframes

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -2,13 +2,17 @@ var express = require('express');
 var router = express.Router();
 var async = require('async');
 var recipe_seeds = require('../json/recipe_seeds.js');
+var recipe_tags = require('../json/recipe_tags.js');
 // Return index page
 router.get('/', function(req, res, next) {
     res.render('index');
 });
 
 router.get('/share', function ( req, res, next ) {
-    res.render('pages/share_recipe');
+    console.log(recipe_tags);
+    res.render('pages/share_recipe', {
+      recipe_tags: recipe_tags,
+    });
 });
 
 router.get('/account', function ( req, res, next ) {
@@ -22,7 +26,9 @@ router.get('/about', function ( req, res, next ) {
 router.get('/dashboard', function ( req, res, next ) {
 
     // console.log(recipe_seeds);
-    res.render('pages/dashboard', { recipe_seeds: recipe_seeds});
+    res.render('pages/dashboard', {
+      recipe_seeds: recipe_seeds,
+    });
 });
 
 router.get('/settings', function ( req, res, next ) {
