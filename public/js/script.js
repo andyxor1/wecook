9b970e6bf78cf498cb2db8e501c128bca2aa455b
Mingbin Li <mingbin1996@gmail.com>
2018-11-08 12:02:58 -0800

    Checkbox onclick handler

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
-window.alert("hi");
+//Trigger quary handler when checkbox are clicked
+$(".filter").on("click", queryHandle);
+
+function queryHandle() {
+    var pack = [];
+    $(".filter").each(function(box){
+        var name = $(this).prop("name");
+        var value = $(this).prop("checked");
+        pack.push({name:name, value:value})
+    })
+    //Pass this pack to query API when we have one
+    console.log(pack);
+}
