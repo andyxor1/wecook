2dbabbe83260337007d41a81163eef3fb3cb82c5
Mingbin Li <mingbin1996@gmail.com>
2018-11-08 15:06:01 -0800

    POST skeleton

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,20 @@
 //Trigger quary handler when checkbox are clicked
 $(".filter").on("click", queryHandle);
 
+//Handler that scan the checkboxes and send their states to POST
 function queryHandle() {
     var pack = [];
-    $(".filter").each(function(box){
+    $(".filter").each(function (box) {
         var name = $(this).prop("name");
         var value = $(this).prop("checked");
-        pack.push({name:name, value:value})
+        pack.push({name: name, value: value})
     })
-    //Pass this pack to query API when we have one
+
+    //Print the package to console
     console.log(pack);
+
+    //POST call and print the response
+    $.post("/find-recipes", pack, function (data, status) {
+        console.log(data);
+    })
 }
--- routes/app_routes.js
+++ routes/app_routes.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var async = require('async');
 var recipe_seeds = require('../json/recipe_seeds.js');
 var recipe_tags = require('../json/recipe_tags.js');
+//var Store = require("../models/store_model");
+
 // Return index page
 router.get('/', function(req, res, next) {
     res.render('index');
@@ -24,7 +26,6 @@ router.get('/about', function ( req, res, next ) {
 });
 
 router.get('/dashboard', function ( req, res, next ) {
-
     // console.log(recipe_seeds);
     res.render('pages/dashboard', {
       recipe_seeds: recipe_seeds,
@@ -34,6 +35,22 @@ router.get('/dashboard', function ( req, res, next ) {
 router.get('/settings', function ( req, res, next ) {
     res.render('pages/settings');
 });
+
+//POST call for recipe query
+router.post('/find-recipes', async function(req, res) {
+    for(var key in req.body) {
+        if(req.body.hasOwnProperty(key)){
+            //do something with e.g. req.body[key]
+            console.log("%s   ======    %s", key,req.body[key]);
+        }
+    }
+
+
+    //let result = await User.findById(userId);
+    //doSomethingElseWith(result);
+    res.json(req.body["Earl's"]);
+})
+
 
 
 module.exports = router;
