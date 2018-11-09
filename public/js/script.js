f40260cbbf4f6ae1eb050b8a40be758f7f2360de
Kaung Yang <kky006@ucsd.edu>
2018-11-08 18:03:54 -0800

    merged ming's code in

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,72 @@
-//Trigger quary handler when checkbox are clicked
-$(".filter").on("click", queryHandle);
+$(document).ready(function() {
 
-//Handler that scan the checkboxes and send their states to POST
-function queryHandle() {
-    var pack = [];
-    $(".filter").each(function (box) {
-        var name = $(this).prop("name");
-        var value = $(this).prop("checked");
-        pack.push({name: name, value: value})
-    })
+  // get the recipes on the board
+  // var recipes = document.getElementById("search_output").innerHTML;
+  // var obj = JSON.parse(recipes);
+  // console.log(obj)
+  // var myVar = <%- JSON.stringify(recipe_seeds) %>;
+  // console.log(myVar)
+  //
+  // console.log("This is recipes");
+  // console.log(recipes);
+  // let recipe_search = elasticlunr(function () {
+  //   this.addField('title');
+  //   this.addField('description');
+  //   this.addField('tags');
+  // });
+  //
+  // for(var recipe in recipes) {
+  //   recipe_title_norm = nlp(recipe.title).normalize().out('text');
+  //   recipe_description_norm = nlp(recipe.description).normalize().out('text');
+  //   recipe_search_obj =
+  //     {
+  //       title: recipe_title_norm,
+  //       description: recipe_description_norm,
+  //       id: recipe._id
+  //     };
+  //   recipe_search.addDoc(recipe_search_obj);
+  // };
 
-    //Print the package to console
-    console.log(pack);
+  // listens to keyup event when being searched in dashboard
+  $('#search_input').on("submit", function() {
+    var searchTerms = $("#search_input").val();
+    console.log(searchTerms);
+    // console.log("Something's being searched");
+    // var x = document.getElementById('recipe_result');
+    // console.log("This is x")
+    // console.log(x)
+    // x.empty();
+    if ( searchTerms != "" ) {
+      console.log(searchTerms);
+      $.ajax({
+          type: "POST",
+          url: "/dashboard",
+          data: {"key": searchTerms },
+          dataType: "text",
+          success: function(data) {
+            console.log("Success!")
+              // if (data.msg == "available") {
+              //     flag = true;
+              // }
+              // else if (data.msg == "used") {
+              //     alert("Email already used, please try a different email.");
+              //     $("#email").show().effect("pulsate", { times: 3 }, 1000);
+              // }
+          },
+          error: function(xhr, ajaxOptions, thrownError) {
+              console.log(xhr.status);
+              console.log(xhr.responseText);
+              console.log(thrownError);
+              alert("An error occured, please try again.");
+          }
+      });
+    } else { console.log("no search terms");}
 
-    //POST call and print the response
-    $.post("/find-recipes", pack, function (data, status) {
-        console.log(data);
-    })
-}
+  });
+
+
+
+
+
+
+});
--- routes/app_routes.js
+++ routes/app_routes.js
@@ -3,9 +3,26 @@ var router = express.Router();
 var async = require('async');
 var recipe_seeds = require('../json/recipe_seeds.js');
 var recipe_tags = require('../json/recipe_tags.js');
-//var Store = require("../models/store_model");
+var elasticlunr = require('elasticlunr');
+var nlp = require('compromise')
+// "require" is similar to import statements
 
-// Return index page
+var mongoose = require('mongoose'); // for mongo db
+var path = require('path'); // for specifying path of directory
+
+
+var Recipe = require('../models/recipe_model');
+var Tag = require('../models/tag_model');
+var bodyParser = require('body-parser'); // for parsing from HTTP message body
+
+
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
+
+
+// ------------------
+// INDEX ROUTES |
+// ------------------
 router.get('/', function(req, res, next) {
     res.render('index');
 });
@@ -17,21 +34,113 @@ router.get('/share', function ( req, res, next ) {
     });
 });
 
-router.get('/account', function ( req, res, next ) {
-    res.render('pages/account');
-});
-
 router.get('/about', function ( req, res, next ) {
     res.render('pages/about');
 });
 
-router.get('/dashboard', function ( req, res, next ) {
-    // console.log(recipe_seeds);
-    res.render('pages/dashboard', {
-      recipe_seeds: recipe_seeds,
-    });
+
+// ------------------
+// ACCOUNT ROUTES |
+// ------------------
+router.get('/account', function ( req, res, next ) {
+    res.render('pages/account');
 });
 
+
+
+// ------------------
+// DASHBOARD ROUTES |
+// ------------------
+router.get('/dashboard', function ( req, res, next ) {
+
+  Recipe.find(function(err, recipes) {
+    if(err) { console.log(err); }
+    console.log("\n\n\n\n\n\n\n\n\n\n\n\n");
+    console.log(req.body);
+    console.log("\n\n\n\n\n\n\n\n\n\n\n\n");
+    res.render('pages/dashboard', {
+      recipe_seeds: recipes,
+    });
+
+  });
+
+
+});
+router.post('/dashboard', function ( req, res, next ) {
+
+  Recipe.find({"title": value }, function(err, recipes) {
+    if(err) { console.log(err); }
+    console.log("\n\n\n\n\n\n\n\n\n\n\n\n");
+    console.log(req.body);
+    console.log("\n\n\n\n\n\n\n\n\n\n\n\n");
+    res.render('pages/dashboard', {
+      recipe_seeds: recipes,
+    });
+
+  });
+
+
+});
+// router.post('/dashboard', function ( req, res, next ) {
+//   console.log("called here")
+//   console.log(req.body);
+//
+//   Recipe.find(function(err, recipes) {
+//     if(err) { console.log(err); }
+//     console.log("hellow");
+//     let recipe_search = elasticlunr(function () {
+//       this.addField('title');
+//       this.addField('description');
+//     });
+//
+//     console.log(recipes)
+//
+//     recipes.forEach(function(recipe) {
+//       recipe_title_norm = nlp(recipe.title).normalize().out('text');
+//       recipe_description_norm = nlp(recipe.description).normalize().out('text');
+//       recipe_search_obj =
+//         {
+//           title: recipe_title_norm,
+//           description: recipe_description_norm,
+//           id: recipe._id
+//         };
+//       recipe_search.addDoc(recipe_search_obj);
+//     });
+//     // console.log(req.body["key"]);
+//
+//     let searchResults = recipe_search.search(req.body["key"], {
+//         fields: {
+//             title: {expand: true},
+//             description: {expand: true}
+//         },
+//         bool: "OR",
+//         expand: true
+//     });
+//
+//     console.log(searchResults);
+//     var newRecipe = [];
+//     searchResults.forEach(function(result) {
+//       var ref = result.ref;
+//       console.log("ID:");
+//       var found = recipes.find(recipe => recipe._id == ref);
+//       console.log("found");
+//       newRecipe.indexOf(found) === -1 ? newRecipe.push(found) : console.log("This item already exists");
+//
+//     })
+//     res.render('pages/dashboard', {
+//       recipe_seeds: newRecipe,
+//     });
+//
+//   });
+//
+//
+// });
+
+
+
+// ------------------
+// SETTINGS ROUTES |
+// ------------------
 router.get('/settings', function ( req, res, next ) {
     res.render('pages/settings');
 });
